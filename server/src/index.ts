75c4a36f8ada0290bb41708d765ed367824610b2
Tejas Nanoti <tejasnanoti2@gmail.com>
2024-02-06 18:45:10 +0530

    chore[server/src/index.ts]:🛠️ Add Cors Config;

--- server/src/index.ts
+++ server/src/index.ts
@@ -16,11 +16,14 @@ import adminRoutes from './routes/adminRoutes'
 
 const app = express()
 
-app.use(cors({
-    credentials: true,
-    origin: process.env.ORIGIN
-}))
-// app.use(cors())
+if (process.env.ORIGIN) {
+    app.use(cors({
+        credentials: true,
+        origin: process.env.ORIGIN
+    }))
+} else {
+    app.use(cors())
+}
 app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
